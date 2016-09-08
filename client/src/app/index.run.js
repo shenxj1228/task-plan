f33e8acac504c5a002e9f652b3261cd80810f2f6
unknown <>
2016-09-08 23:24:42 +0800

    修改

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -13,20 +13,22 @@
         $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
             if (toState.redirectTo) {
                 event.preventDefault();
-                $state.go(toState.redirectTo, toParams, { location: 'replace' })
+                $state.go(toState.redirectTo, toParams, { location: 'replace' });
             }
+            
             if ((toState.access && toState.access.requiredLogin) && !AuthenticationFactory.isLogged) {
                 $state.go('signin');
             } else {
                 // check if user object exists else fetch it. This is incase of a page refresh
                 if (!AuthenticationFactory.user) AuthenticationFactory.user = $window.sessionStorage.user;
                 if (!AuthenticationFactory.userRole) AuthenticationFactory.userRole = $window.sessionStorage.userRole;
+
             }
         });
         $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
             $rootScope.role = AuthenticationFactory.userRole;
             // if the user is already logged in, take him to the home page
-            if (AuthenticationFactory.isLogged == true && $state.includes('signin')) {
+            if (AuthenticationFactory.isLogged === true && $state.includes('signin')) {
                 $state.go('home.warn');
             }
         });
